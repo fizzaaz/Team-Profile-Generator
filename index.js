ecf6905642d8932e80f432725077ce63bdd77ccd
unknown <fizz.zehra14@gmail.com>
2021-07-16 17:41:32 -0500

    prompting user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //importing required libraries for filing and prompting user
 const fs = require("fs");
-const inquirer =require("inquirer");
+const inquirer = require("inquirer");
 
 //importing classes
 const manager = require('./lib/Manager')
@@ -14,28 +14,85 @@ const questions = [
     {
         type: "input",
         name: "id",
-        message: "Enter Employee's ID: (*)"
+        message: "Enter Employee's ID: (*)",
+        validate: validID => {
+            if (validID) {
+                let id = parseInt(validID);
+                if (Number.isInteger(id)) {
+                    return true;
+                }
+                else {
+                    console.log("ID must be 0-9 digits only");
+                    return false;
+
+                }
+            }
+            else {
+                console.log("ID is required");
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "name",
-        message: "Enter Employee's name: (*)"
+        message: "Enter Employee's name: (*)",
+        validate: validName => {
+            if (validName) {
+                let letters = /^[A-Za-z]+$/;
+                if (validName.match(letters)) {
+                    return true;
+                }
+                else {
+                    console.log("Name should have alphabets only")
+                    return false;
+                }
+
+            }
+            else {
+                console.log("Employee's name is required");
+
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "email",
-        message: "Enter Employee's email address: (*)"
+        message: "Enter Employee's email address: (*)",
+        validate: validEmail=>
+        {
+            if(validEmail)
+            {
+                let email=/\S+@\S+\.\S+/;
+                if(validEmail.match(email))
+                {
+                    return true;
+                }
+                else{
+                    console.log("Invalid Emaill Address");
+
+                }
+
+            }
+            else
+            {
+console.log("Email is required!");
+return false;
+            }
+
+        }
     },
     {
-        type: "input",
-        name: "id",
-        message: "Select the ${'name'} role: (*)"
+        type: "checkbox",
+        name: "role",
+        message: "Select the Employee's role: (*)",
+        choices: ['Engineer', 'Manager', 'Intern']
     }
 ]
 
-const init=()=>
-{
-inquirer.prompt("how jsjw");
+const init = () => {
+    inquirer.prompt(questions);
 }
 
 init();
