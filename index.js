f8b39c32da5fd5e37121a42438774b3d8ada04b0
unknown <fizz.zehra14@gmail.com>
2021-07-16 15:41:16 -0500

    now create index.js

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 //importing required libraries for filing and prompting user
-const fs=require("fs");
-const inquirer=require("inquirer");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
 //importing classes
-const manager=require('./lib/Manager')
-const intern=require('./lib/Intern')
-const engineer=require('./lib/Engineer')
+const manager = require('./lib/Manager')
+const intern = require('./lib/Intern')
+const engineer = require('./lib/Engineer')
 
 //All team members data storage
-const teamMembers=[];
+const teamMembers = [];
 
+const questions = [
+    {
+        type: "input",
+        name: "id",
+        message: "Enter Employee's ID: (*)"
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Enter Employee's name: (*)"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter Employee's email address: (*)"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Select the ${'name'} role: (*)"
+    }
+]
 
+const init=()=>
+{
+inquirer.prompt(questions);
+}
+
+init();
