6ac866ba8df54ab917265ce43fe1a01846422b26
unknown <fizz.zehra14@gmail.com>
2021-07-16 14:45:44 -0500

    importing packages

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+//importing required libraries for filing and prompting user
 const fs=require("fs");
 const inquirer=require("inquirer");
+
+//All team members data storage
+const teamMembers=[];
+
+
