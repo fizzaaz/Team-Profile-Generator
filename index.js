62adfbcabf95662d5246579c30ffd8302add4f44
unknown <fizz.zehra14@gmail.com>
2021-07-16 15:27:57 -0500

    now create index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 const fs=require("fs");
 const inquirer=require("inquirer");
 
+//importing classes
+const manager=require('./lib/Manager')
+const intern=require('./lib/Intern')
+const engineer=require('./lib/Engineer')
+
 //All team members data storage
 const teamMembers=[];
 
