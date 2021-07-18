0a982271b18145e56f8475b19244e82a6e1c656a
unknown <fizz.zehra14@gmail.com>
2021-07-18 08:40:24 -0500

    TESTING EMPLOYEE

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,29 @@
+
 const Employee = require("../lib/Employee");
 
 //check if can employee object is working //passed
-describe("Employee", () => {
-    it("Can instantiate Employee instance", () => {
+    test("Can instantiate Employee instance", () => {
         const e = new Employee();
         expect(typeof(e)).toBe("object");
     });
-    it("Can set id via constructor arguments", () => {
-        const e = new Employee(5);
-        expect(e.id).toBe(5);
+
+ //check if can employee can set employee it with constrctor
+    test("Can set id via constructor arguments", () => {
+        testId=5;
+        const emp = new Employee(testId);
+        expect(emp.id).toBe(testId);
     });
-});
+
+   //check if the number is numbers only  //passed
+     test("Id must be numbers only", () => {
+    const emp = new Employee(5);
+    expect(emp.id).toEqual(expect.any(Number));
+    });
+
+    test("Can set name via constructor arguments",()=>
+    {
+testName="Fizza";
+const emp=new Employee(5,testName);
+expect(emp.name).toEqual(testName)
+    });
+
